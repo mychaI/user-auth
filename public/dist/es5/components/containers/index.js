a701837791d63c223f113399dff81d448a95c892
volta tumbleweed <you@example.com>
2018-07-29 15:40:09 -0700

    create Auth container

---
+++ public/dist/es5/components/containers/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * *
+	Export your container components here. The Users
+	container is just an example and you will likely
+	remove it in favor of your own containers. 
+* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*/
+
+var Auth = _interopRequire(require("./Auth"));
+
+exports.Auth = Auth;
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
---
+++ public/dist/es5/utils/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+
+var TurboClient = _interopRequire(require("./TurboClient"));
+
+var ServerEntry = _interopRequire(require("./ServerEntry"));
+
+var renderComponents = _interopRequire(require("./renderComponents"));
+
+exports.TurboClient = TurboClient;
+exports.ServerEntry = ServerEntry;
+exports.renderComponents = renderComponents;
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
